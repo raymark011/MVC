0ba018783b90b0e89e3b7bd737c5aa131b405cc6
raymark011 <rcabriana.shs@gmail.com>
2020-03-15 04:08:12 +0800

    first commit

---
+++ Models/Book.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Models
+{
+    public class Book
+    {
+        public int ID { get; set; }
+        public String Title { get; set; }
+        public String Description { get; set; }
+        public String Author { get; set; }
+        public String Publisher { get; set; }
+        public String DatePublished { get; set; }
+        public String BookFormat { get; set; }
+    }
+}
